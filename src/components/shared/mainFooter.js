adf10223ab7030788443565e5138dacd971956eb
tendai pfidze <pfidzetendai@gmail.com>
2021-01-16 22:38:21 +0200

    refactoring to mysql

--- src/components/shared/mainFooter.js
+++ src/components/shared/mainFooter.js
@@ -3,8 +3,8 @@ import React from 'react'
 const MainFooter = () => {
     return (
         <div className="row footer my-0 py-0">
-            <div className="col-12 my-2 py-2 text-center my-0 py-0">
-                <small className="font-italic my-0 py-0">Hippocrates Health Alliance | Health access for all | <i className="fas fa-copyright"></i> 2020  Software developed by Tendai P. Pfidze</small>
+            <div className="col-12 my-1 py-1 text-center">
+                <small className="font-italic">Hippocrates Health Alliance | Health access for all | <i className="fas fa-copyright"></i> 2020  Software developed by Tendai P. Pfidze</small>
             </div>
         </div>
     )
